8db30dd505856cf5186f5d68c8eee946b623c0c7
Simon Voglimacci <44271817+svoglimacci@users.noreply.github.com>
2024-02-20 19:49:36 -0500

    Update PrimeCalculator.cpp

--- TP1/Q2 nth prime/PrimeCalculator.cpp
+++ TP1/Q2 nth prime/PrimeCalculator.cpp
@@ -1,21 +1,53 @@
-//Nom, Matricule
-//Nom, Matricule
+//Simon Voglimacci Stephanopoli, 20002825
+//Julie Yang, 20239909
 
 #include "PrimeCalculator.h"
-// #include <vector>
-// #include <math.h>
+#include <stdio.h>
+#include <vector>
+#include <math.h>
 
 // ce fichier contient les definitions des methodes de la classe PrimeCalculator
 // this file contains the definitions of the methods of the PrimeCalculator class
 
+
 PrimeCalculator::PrimeCalculator()
 {
+
+}
+
+//https://en.wikipedia.org/wiki/Primality_test
+bool isPrime(int N)
+{
+    if (N <= 1)
+        return false;
+
+    if (N == 2 || N == 3)
+        return true;
+
+    if ( N % 2 == 0 || N % 3 == 0)
+        return false;
+
+    for (int i = 5; sqrt(N) >= i; i += 6)
+    {
+        if (N % i == 0 || N % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
 }
 
 int PrimeCalculator::CalculateNthPrime(int N)
 {
-    // a completer
-    // TODO
-
-    return 0;
+  int count = 0;
+    int i;
+    for (i = 2; ; i++)
+    {
+        if (isPrime(i))
+        {
+            count++;
+            if (count == N)
+                break;
+        }
+    }
+    return i;
 }
